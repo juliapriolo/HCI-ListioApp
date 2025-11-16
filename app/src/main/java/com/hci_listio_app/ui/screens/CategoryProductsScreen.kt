d25ec6a729e571a03e9eff915d9be9b5c960fbd9
juliapriolo <juliapriolo9@gmail.com>
2025-11-16 16:48:18 -0300

    Update CategoryProductsScreen.kt

--- app/src/main/java/com/hci_listio_app/ui/screens/CategoryProductsScreen.kt
+++ app/src/main/java/com/hci_listio_app/ui/screens/CategoryProductsScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.ui.graphics.Color
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -11,7 +12,6 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
